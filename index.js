7052fce8e2a563dea242d713cd90919f5cc1c03a
Rahul Shetty <Rahul.Shetty@nike.com>
2021-08-20 03:43:12 +0530

    Start using docker compose

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 // Require the framework and instantiate it
 const fastify = require("fastify")({ logger: true });
+const redis = require("redis");
+const client = redis.createClient({
+  host: "redis-server",
+  port: 6379,
+});
+client.set("visits", 0);
 
 // Declare a route
-fastify.get("/", async (request, reply) => {
-  return { hello: "world" };
+fastify.get("/", async (req, res) => {
+  client.get("visits", (err, visits) => {
+    if (err) return err;
+
+    res.send("Number of visits:" + visits);
+    client.set("visits", parseInt(visits) + 1);
+  });
 });
 
 // Run the server!
