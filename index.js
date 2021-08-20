32aa96d83232698c34b8ae1de00ba62f6ed4038a
Rahul Shetty <Rahul.Shetty@nike.com>
2021-08-20 20:17:48 +0530

    Extend eslint using prettier plugin

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 // Require the framework and instantiate it
-const fastify = require("fastify")({ logger: true });
-const redis = require("redis");
+const fastify = require('fastify')({ logger: true });
+const redis = require('redis');
 const client = redis.createClient({
-  host: "redis-server",
+  host: 'redis-server',
   port: 6379,
 });
-client.set("visits", 0);
+client.set('visits', 0);
 
 // Declare a route
-fastify.get("/", async (req, res) => {
-  client.get("visits", (err, visits) => {
+fastify.get('/', async (req, res) => {
+  client.get('visits', (err, visits) => {
     if (err) return err;
 
-    res.send("Number of visits:" + visits);
-    client.set("visits", parseInt(visits) + 1);
+    res.send('Number of visits:' + visits);
+    client.set('visits', parseInt(visits) + 1);
   });
 });
 
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(3000, "0.0.0.0");
+    await fastify.listen(3000, '0.0.0.0');
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
