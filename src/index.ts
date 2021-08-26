1f6ca1c1a1f67b56dd59d9694c15d2c16ea81273
Rahul Shetty <shettyrahul8june@gmail.com>
2021-08-27 04:39:54 +0530

    Stop all process gracefully on end signal

--- src/index.ts
+++ src/index.ts
@@ -22,10 +22,24 @@ server.get('/', async (_req, res) => {
 const start = async () => {
   try {
     await server.listen(3000, '0.0.0.0');
+    console.info(`*^!@4=> Process id: ${process.pid}`);
   } catch (err) {
     server.log.error(err);
     process.exit(1);
   }
 };
+
+async function closeGracefully(signal: NodeJS.Signals) {
+  console.info(`*^!@4=> Received signal to terminate: ${signal}`);
+
+  await server.close();
+
+  // await db.close() if we have a db connection in this app
+  // await other things we should cleanup nicely
+  process.exit();
+}
+
+process.on('SIGINT', closeGracefully);
+process.on('SIGTERM', closeGracefully);
 
 start();
