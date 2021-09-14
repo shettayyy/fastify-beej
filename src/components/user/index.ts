77cd122008e1bc5e06df58eb035d262c678d9d7f
Rahul Shetty <shettyrahul8june@gmail.com>
2021-09-14 08:13:52 +0530

    Update the routes

---
+++ src/components/user/index.ts
@@ -0,0 +1,3 @@
+export const getUser = async (): Promise<{ hello: string }> => {
+  return { hello: 'world' };
+};
--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,15 @@
+import { getUser } from './components/user/index';
 import { FastifyInstance } from 'fastify';
 
 export const PREFIX = 'api/v1';
 
-export const routes = (server: FastifyInstance): Promise<void> => {
+export const routes = (server: FastifyInstance): void => {
   server.addHook('preHandler', async (_req, res) => {
     // do something on api routes
     if (res.sent) return false; // stop on error (like user authentication)
   });
 
-  server.get('/', async () => {
-    return { hello: 'world' };
-  });
+  server.get('/user', getUser);
 
   // only for authenticated users with role.
   server.register(async role => {
@@ -23,6 +22,4 @@ export const routes = (server: FastifyInstance): Promise<void> => {
       return { hello: 'world' };
     });
   });
-
-  return Promise.resolve();
 };
